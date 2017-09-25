c7edf3cf7c1da65ed57b9d7d3bf71a5159343bb8
Aparna <aparna.tiwari@omni-bridge.net>
2017-09-25 21:59:38 +0530

    Initial project with common Required pods.

---
+++ ShoppingApp/ShoppingApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ShoppingApp
+//
+//  Created by Aparna Tiwari on 9/25/17.
+//  Copyright © 2017 AT. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
